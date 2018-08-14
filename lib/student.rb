d9a8dc09e1d586aef34a2dedaf0fc2f11c720085
Neena van de Water <nkuriakose@hotmail.com>
2018-08-14 19:17:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      student.new(students_array)
+      Student.new(student_hash)
     end
 
   end
